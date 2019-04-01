fb74f7c061c08d7a8740c7ee40326fc6a26b336f
Jonathan Sharman <jonathan.sharman@gmail.com>
2019-03-31 21:02:32 -0500

    Added in-game instructions. Release.

--- docs/resources.js
+++ docs/resources.js
@@ -1,3 +1,3 @@
-﻿C$common$addTextRes('image_sheets.txt', "S,0,twocans_everything");
-C$common$resourceManifest = "";
+﻿C$common$addTextRes('image_sheets.txt', "S,0,twocans_everything\nC,0,0\nT,t1.png,0,0,256,256,0\nT,t2.png,256,0,256,256,0\nI,0,0,281,99,instructions.png");
+C$common$resourceManifest = "IMGSH,instructions.png,,twocans_everything";
 C$common$jsFilePrefix = "";
